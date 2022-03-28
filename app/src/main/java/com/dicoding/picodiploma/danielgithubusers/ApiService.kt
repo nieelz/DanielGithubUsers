7e7333c07a25d50ef63b8f0fda8acb806c481c3e
nieelz <nieelz.id@gmail.com>
2022-03-28 15:43:00 +0700

    API

--- app/src/main/java/com/dicoding/picodiploma/danielgithubusers/ApiService.kt
+++ app/src/main/java/com/dicoding/picodiploma/danielgithubusers/ApiService.kt
@@ -5,25 +5,25 @@ import retrofit2.http.*
 
 interface ApiService {
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getUser(
         @Query("q") username: String
     ): Call<UserResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getDetail(
         @Path("username") username: String
     ): Call<DetailUserResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowing(
         @Path("username") following: String
     ): Call <ArrayList<FollowUserResponseItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token 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")
     fun getFollowers(
         @Path("username") followers: String
     ): Call <ArrayList<FollowUserResponseItem>>
